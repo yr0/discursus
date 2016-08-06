5b164a3aa7574d123ffdaa3d57b77f4e8c04ef5b
Yaroslav <numberonename@gmail.com>
2016-08-07 02:49:04 +0300

    [~] Fix spinner animation, add logic for displaying spinner for a minimum amount of time

--- app/assets/javascripts/ajax_spinner.js
+++ app/assets/javascripts/ajax_spinner.js
@@ -1,27 +1,37 @@
+// There can only be one spinner within a div - sibling spinners will not work
 (function(W, D, $){
+    var spinnerDisplayMinTime = 1000; // display spinner for a minimum of 1 second
     $(D).on('turbolinks:load', function() {
-        $('body').on('click', '[data-with-ajax-spinner]', function(event) {
-            displaySpinner.call(this, event);
-        }).on('ajax:success', '[data-with-ajax-spinner]', function() {
-            setTimeout(hideSpinner.bind(this), 2000);
+        $('body').on('ajax:before', '[data-with-ajax-spinner]', function() {
+            displaySpinner.apply(this);
+        }).on('ajax:templateLoaded', '[data-with-ajax-spinner]', function() {
+            hideSpinner.apply(this);
         });
     });
 
-    function displaySpinner(event) {
+    function displaySpinner() {
         hide($(this));
-        var clone = $('#dscSpinnerTemplate').clone()
-            .attr('id', 'dscSpinner')
-            .css({
-                display: $('#dscSpinnerTemplate').data('display'),
-                top: event.pageY
-            });
-        show(clone.insertBefore($(this)));
+        var $spinner = $(this).siblings('.dsc-spinner:first'),
+            spinnerStarted = Date.now();
+        if (!$spinner.length) {
+            $spinner = $('#dscSpinnerTemplate').clone().insertBefore($(this));
+            //apply the scale property so that initial animation is fired
+            W.getComputedStyle($spinner.get(0)).getPropertyValue('transform');
+        }
+        // save start spinner time in order to hide it at least after spinnerDisplayMinTime milliseconds
+        $spinner.attr('id', 'spinner' + spinnerStarted);
+        $(this).data('spinner-started', spinnerStarted);
+        show($spinner);
     }
 
     function hideSpinner() {
-        //hide($('#dscSpinner'));
-        //show($(this));
-        //$('#dscSpinner').remove();
+        var initialElement = $(this),
+            spinnerStarted = initialElement.data('spinner-started');
+        setTimeout(function(){
+            hide($('#spinner' + spinnerStarted));
+            show(initialElement);
+        }, getSpinnerDelay(spinnerStarted));
+        $spinner.data('spinner-started')
     }
 
     function show($element) {
@@ -30,6 +40,11 @@
 
     function hide($element) {
         $element.removeClass('dsc-appearing').addClass('dsc-disappearing');
+    }
+
+    function getSpinnerDelay(startTime) {
+        var result = spinnerDisplayMinTime - (Date.now() - startTime);
+        return result > 0 ? result : 0;
     }
 
     // button_to generates form, so final element is a submit within the form, while link_to generates a link
