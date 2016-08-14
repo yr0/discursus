be64fe009cdfe830f0c4c6f65ea15a1dc0939412
Yaroslav <numberonename@gmail.com>
2016-08-14 03:59:23 +0300

    [+] Add sidenav navigation and its styles, make navigation helper reusable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,21 @@
 module ApplicationHelper
   START_YEAR = 2016
+  NAVIGATION = {
+    main: '', books: '#', news: '#', bookstores: '#', authors: '#', about_us: '#', contacts: '#'
+  }.freeze
 
   def years_active
     [START_YEAR, Time.zone.now.year].uniq.join('&ndash;').html_safe
+  end
+
+  def site_navigation(css_class_infix, active_li = :main)
+    content_tag :ul, class: "dsc-#{css_class_infix}-nav-items" do
+      NAVIGATION.each do |item_name, route|
+        link_class = "dsc-#{css_class_infix}-nav-link"
+        link_class += ' active' if item_name == active_li
+        concat content_tag(:li, link_to(I18n.t("nav.#{item_name}").gsub(' ', '&nbsp;').html_safe, url_for(route),
+                                        class: link_class), class: "dsc-#{css_class_infix}-nav-item")
+      end
+    end
   end
 end
