8273d6affde8c89f4d5a79a6cc00bf7074d45732
yaro <numberonename@gmail.com>
2017-09-28 22:49:14 +0300

    [~] Fix #show action for authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,6 @@
 class AuthorsController < ApplicationController
+  load_resource find_by: :slug, only: :show
+
   def index
     @authors = Author.includes(:books).for_index.page(params[:page]).per(8)
     respond_to :html, :js
