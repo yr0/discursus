e3fd59c56a5aacb655719ac20aafee3a2b3e56f0
yaro <numberonename@gmail.com>
2017-09-29 22:07:23 +0300

    [+] Add fallback locale for application

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Discursus
   class Application < Rails::Application
     config.i18n.available_locales = %i(uk en)
     config.i18n.default_locale = :uk
+    config.i18n.fallbacks = [:en]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.time_zone = 'Kyiv'
     config.action_view.embed_authenticity_token_in_remote_forms = true
