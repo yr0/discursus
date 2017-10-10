49c90394d33ca3df729ed8176a395436d89411a2
yaro <numberonename@gmail.com>
2017-10-10 23:25:51 +0300

    [~] Move staging settings to production

---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+server '94.130.73.140', roles: %w(app web db), primary: true, user: 'deploy'
+
+set :deploy_to, '/home/deploy/discursus'
+set :rails_env, 'production'
+
+after 'deploy:finished', 'discursus:restart'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
-server '94.130.73.140', roles: %w(app web db), primary: true, user: 'deploy'
+# server '94.130.73.140', roles: %w(app web db), primary: true, user: 'deploy'
 
-set :deploy_to, '/home/deploy/discursus'
+set :deploy_to, '/home/deploy/discursus_staging'
 set :branch, :dev
 set :rails_env, 'production'
 
