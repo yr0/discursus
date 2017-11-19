11a2a0a101dd863bfac9414afac6e43d9db390d1
Yaro Holodiuk <pragmateme@gmail.com>
2017-11-19 22:48:24 +0200

    [~] Modify default from to include the name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   add_template_helper ApplicationHelper
 
-  default from: 'sales@discursus.com.ua', reply_to: 'discursus.sales@gmail.com'
+  default from: 'Discursus <sales@discursus.com.ua>', reply_to: 'discursus.sales@gmail.com'
   layout 'mailer'
 end
