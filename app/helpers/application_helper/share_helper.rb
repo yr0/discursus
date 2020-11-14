75a54641d0d74d877acf0eeb2cfdf1ad45a08437
Yaro Holodiuk <pragmateme@gmail.com>
2020-11-14 18:59:11 +0200

    Fix formatting

--- app/helpers/application_helper/share_helper.rb
+++ app/helpers/application_helper/share_helper.rb
@@ -3,19 +3,19 @@
 module ApplicationHelper
   module ShareHelper
     AVAILABLE_SHARE_ACTIONS = [
-    ['facebook', ->(_title, link) { "https://www.facebook.com/sharer/sharer.php?u=#{CGI.escape(link)}" }],
-    ['twitter', lambda do |title, link|
-      "http://twitter.com/share?text=#{CGI.escape(title)}&url=#{CGI.escape(link)}&hashtags=discursus"
-    end],
-    ['envelope', ->(title, link) { "mailto:?subject=#{CGI.escape(title)}&body=#{CGI.escape(link)}" }]
-  ].freeze
+      ['facebook', ->(_title, link) { "https://www.facebook.com/sharer/sharer.php?u=#{CGI.escape(link)}" }],
+      ['twitter', lambda do |title, link|
+        "http://twitter.com/share?text=#{CGI.escape(title)}&url=#{CGI.escape(link)}&hashtags=discursus"
+      end],
+      ['envelope', ->(title, link) { "mailto:?subject=#{CGI.escape(title)}&body=#{CGI.escape(link)}" }]
+    ].freeze
 
-  def share_icons_per(title, link)
-    AVAILABLE_SHARE_ACTIONS.each do |icon, share_action|
-      concat link_to(fa_stacked_icon("#{icon} inverse",
-                                     base: 'stop 2x', class: 'dsc-share-icon fa-2x', target: '_blank'),
-                     share_action.call(title, link))
+    def share_icons_per(title, link)
+      AVAILABLE_SHARE_ACTIONS.each do |icon, share_action|
+        concat link_to(fa_stacked_icon("#{icon} inverse",
+                                       base: 'stop 2x', class: 'dsc-share-icon fa-2x', target: '_blank'),
+                       share_action.call(title, link))
+      end
     end
-  end
   end
 end
